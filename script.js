0dd380bf9cd358465f05f78c81edfc5ccefcb120
Renan Giaretta <110490586+renangiaretta@users.noreply.github.com>
2022-09-02 23:13:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+let listaPainting = [
+    {
+        categoria: 'Paintings',
+        imagem: 'assets/painting/clock.jpg',
+        nome: 'Relógio Smart',
+        preco: 'R$ 497.00'
+    },
+    {
+        categoria: 'Paintings',
+        imagem: 'assets/painting/gamepad.jpg',
+        nome: 'Controle ps5',
+        preco: 'R$ 250.00'
+    },
+    {
+        categoria: 'Paintings',
+        imagem: 'assets/painting/personagem.jpg',
+        nome: 'Mr. Robot',
+        preco: 'R$ 12500.00'
+    }
+]
+
+let listaActionFigure = [
+    {
+        categoria: 'Action Figure',
+        imagem: 'assets/actions/animewoman.jpg',
+        nome: 'Garota',
+        preco: 'R$ 1.99'
+    },
+    {
+        categoria: 'Action Figure',
+        imagem: 'assets/actions/dragonballpersonagem.jpg',
+        nome: 'Goku',
+        preco: 'R$ 49.90'
+    },
+    {
+        categoria: 'Action Figure',
+        imagem: 'assets/actions/starwarspersonagem.jpg',
+        nome: 'Baby Yoda',
+        preco: 'R$ 39.90'
+    }
+]
+
+function createActionItem(categoria, imagem, nome, preco){
+    if(categoria == 'Action Figure'){
+        let objeto = {
+            categoria: categoria,
+            imagem: imagem,
+            nome: nome,
+            preco: preco
+        }
+        listaActionFigure.push(objeto)
+        return objeto
+    }
+}
+createActionItem()
+// console.log(listaActionFigure)
+
+function createPaintingIem(categoria, imagem, nome, preco){
+    if(categoria == 'Paintings'){
+        let objeto = {
+            categoria: categoria,
+            imagem: imagem,
+            nome: nome,
+            preco: preco
+        }
+        listaPainting.push(objeto)
+        return objeto
+    }
+}
+createPaintingIem()
+let secaoListaPainting = document.getElementsByClassName('paint')
+let secaoListaFigure = document.getElementsByClassName('action')
+
+for(let i = 0; i < listaPainting.length; i++){
+
+    let elementoLista = document.createElement('li')
+    let imagem = document.createElement('img')
+    let nome = document.createElement('p')
+    let preco = document.createElement('span')
+
+    imagem.src = listaPainting[i].imagem
+    nome.innerText = listaPainting[i].nome
+    preco.innerText = listaPainting[i].preco
+    elementoLista.append(imagem, nome, preco)
+    secaoListaPainting[0].appendChild(elementoLista)
+}
+
+for(let i = 0; i < listaActionFigure.length; i++){
+  
+    let elementoLista = document.createElement('li')
+    let imagem = document.createElement('img')
+    let nome = document.createElement('p')
+    let preco = document.createElement('span')
+    // console.log(elementoLista)
+
+    imagem.src = listaActionFigure[i].imagem
+    nome.innerText = listaActionFigure[i].nome
+    preco.innerText = listaActionFigure[i].preco
+    elementoLista.append(imagem, nome, preco)
+    secaoListaFigure[0].appendChild(elementoLista)
+}
